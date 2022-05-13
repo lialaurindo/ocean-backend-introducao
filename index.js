12b931bbc6b9a9fd8abc85c4624713b447797bef
lialaurindo <liamaralaurindo@gmail.com>
2022-05-13 12:07:07 -0300

    integrando com banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const url = 'mongodb://localhost:27017'
+const dbName = 'ocean_bancodedados_13_05_2022';
+
+async function main () {
+console.log ('Conectando com banco de dados...')
+const client = await MongoClient.connect (url);
+const db = client.db(dbName);
+const collection = db.collection(herois);
+console.log ('Conexão com o banco de dados realizada com sucesso');
+
 const app = express()
 
 //indica para o Express que estamos usando Json
 app.use(express.json());
+
 
 
 //criando a primeira rota da aplicação
@@ -11,14 +24,15 @@ app.get('/', function (req, res) {
 });
 
 
-//Incluindo uma lista de heróis para ser utilizada em novas rotas
+//Declarando uma lista de heróis para ser utilizada em novas rotas
 const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"]
 
 
 // Nova rota: Read All > ler todos os itens da lista de heróis
-app.get('/herois', function (req, res) {
+app.get('/herois', async function (req, res) {
   //Exibindo a lista e incluindo o .filter(boolean) para só exibir itens com valor
-  res.send(herois.filter(Boolean))
+  const documentos = await collection.find().toArray();
+  res.send(documentos)
 });
 
 
@@ -26,7 +40,7 @@ app.get('/herois', function (req, res) {
 app.get('/herois/:id', function (req, res) {
   //Recebemos o ID que iremos buscar
   const id = req.params.id;
-  const item = herois[id - 1];
+  const item = await collection.findOne({_id: new ObjectId(id)});
 
   //incluindo a lógica para id não encontrado na lista 
   if (!item) {
@@ -41,9 +55,9 @@ app.get('/herois/:id', function (req, res) {
 
 
 //Create: criar um novo item na lista
-app.post('/herois', function (req, res) {
+app.post('/herois', async function (req, res) {
   //pegando somente o nome do herói
-  const item = req.body.nome;
+  const item = req.body;
 
   if (!item) {
     res.status(400).send("Informar a propriedade 'nome' no corpo da requisição");
@@ -53,31 +67,43 @@ app.post('/herois', function (req, res) {
   }
 
   //Adicionando item obtido dentro da lista de heróis
-  herois.push(item)
-  res.send('Item criado com sucesso');
+  await collection.insertOne(item);
+  res.send(item);
 });
 
 
 //Update (editar um item)
-app.put('/herois/:id', function (req, res) {
+app.put('/herois/:id', async function (req, res) {
   // como vou identificar o que será editado - obtendo ID
   const id = req.params.id;
   // o que será editado - pegamos a nova informação
+
+  const itemEncontrado = await collection.findOne({_id: new ObjectId(id)});
+
+// pegamos a nova informação que está sendo enviada
   const item = req.body.nome;
 
   //atualizamos a informação na lista
-  herois[id - 1] = item;
+  collection.updateOne(
+    {_id: new ObjectId(id)},
+    {
+      $set: item,
+    }
+  );
 
-  res.send('Item editado com sucesso')
+  res.send(item)
 });
 
 
 //Delete (remover um item)
-app.delete('/herois/:id', function (req, res) {
+app.delete('/herois/:id', async function (req, res) {
   //Obtemos o ID do item que será removido
   const id = req.params.id;
 
-  if (!herois[id - 1]) {
+const itemEncontrando = await collection.findOne({_id: new ObjectId(id)});
+
+
+  if (itemEncontrado) {
     // Envia uma resposta de não encontrado
     res.status(404).send("Item não encontrado.");
 
@@ -86,10 +112,14 @@ app.delete('/herois/:id', function (req, res) {
   }
 
   //Removemos o item da lista
-  delete herois[id - 1];
+ await collection.deleteOne ({_id: new ObjectId(id)});
   res.send("Item removido com sucesso")
 })
 
 
 //Incluindo o console.log apenas para dar retorno no terminal quando rodar 
 app.listen(3000, () => console.log('aplicação rodando em localhost:3000'))
+
+}
+
+main (); 
